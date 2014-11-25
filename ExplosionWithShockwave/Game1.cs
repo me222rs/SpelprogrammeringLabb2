f5ea6f7ab375e74ccc08b81860d052382f961048
me222rs <me222rs@student.lnu.se>
2014-11-25 11:31:39 +0100

    4 klar

---
+++ ExplosionWithShockwave/Game1.cs
@@ -0,0 +1,116 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using Particles.Content.View;
+using Explosion.Content;
+using ExplosionWithShockwave.Content.View;
+#endregion
+
+namespace ExplosionWithShockwave
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SplitterSystem splitterSystem;
+        Texture2D particleTexture;
+        Texture2D explosionTexture;
+        AnimateExplosion explosion;
+        ShockWave shockwave;
+        Viewport viewPort;
+        Vector2 position;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 512;
+            graphics.PreferredBackBufferHeight = 512;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+            position.X = 0.5f;
+            position.Y = 0.5f;
+            splitterSystem = new SplitterSystem(GraphicsDevice.Viewport, position);
+            shockwave = new ShockWave(GraphicsDevice.Viewport, position, Content);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            particleTexture = Content.Load<Texture2D>("splitter");
+            explosionTexture = Content.Load<Texture2D>("explosion");
+
+            explosion = new AnimateExplosion(explosionTexture, GraphicsDevice.Viewport);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            splitterSystem.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            shockwave.Draw(spriteBatch, (float)gameTime.ElapsedGameTime.TotalSeconds);
+            splitterSystem.Draw(spriteBatch, particleTexture);
+            explosion.Draw(spriteBatch, (float)gameTime.ElapsedGameTime.TotalSeconds);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
